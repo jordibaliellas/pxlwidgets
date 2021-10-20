27720e0a732c8a58ff802a1f8f1fb8158038d9c0
Jordi Baliellas <jbaliellas@inno-it.es>
2021-10-20 23:15:29 +0200

    Allow cors on backend

--- packages/backend-api/src/router.ts
+++ packages/backend-api/src/router.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import { cacheMiddleware } from "./middlewares/cache.middleware";
 import { rijksmuseumRouter } from "./routes/rijksmuseum.router";
+import cors from "cors";
 
 export const router = express.Router();
 
+router.use(cors());
 router.use(cacheMiddleware);
 router.use("/rijksmuseum", rijksmuseumRouter);

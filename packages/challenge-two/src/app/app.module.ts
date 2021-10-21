e892354fb2e24a19e5f17d83e5f2668f78cd31b8
Jordi Baliellas <jbaliellas@inno-it.es>
2021-10-21 22:46:19 +0200

    add pagination

--- packages/challenge-two/src/app/app.module.ts
+++ packages/challenge-two/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TableComponent } from './components/table/table.component';
+import { PaginationComponent } from './components/pagination/pagination.component';
 
 @NgModule({
-  declarations: [AppComponent, TableComponent],
+  declarations: [AppComponent, TableComponent, PaginationComponent],
   imports: [BrowserModule, HttpClientModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ packages/challenge-two/src/app/components/pagination/pagination.component.ts
@@ -0,0 +1,96 @@
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  Output,
+} from '@angular/core';
+
+const TOTAL_SELECTOR_PAGES = 4;
+const FIRST_PAGE = 1;
+
+@Component({
+  selector: 'app-pagination',
+  templateUrl: './pagination.component.html',
+  styleUrls: ['./pagination.component.scss'],
+})
+export class PaginationComponent implements OnChanges {
+  @Input() skip: number;
+  @Input() limit: number;
+  @Input() total: number;
+  @Output() clickPage = new EventEmitter();
+
+  lastPage: number;
+  currentPage: number;
+  isFirstPage: boolean;
+  isLastPage: boolean;
+
+  pages: number[];
+
+  constructor() {}
+
+  ngOnChanges(): void {
+    this.calculatePages();
+  }
+
+  calculatePages() {
+    if (!this.total) return;
+
+    this.currentPage = this.skip / this.limit + 1;
+    this.lastPage = Math.ceil(this.total / this.limit);
+
+    this.isFirstPage = this.currentPage === FIRST_PAGE;
+    this.isLastPage = this.currentPage === this.lastPage;
+
+    this.pages = [this.currentPage];
+    if (!this.isFirstPage) this.pages.unshift(this.currentPage - 1);
+    if (this.isLastPage) {
+      this.addPagesStart();
+      return;
+    }
+    this.addPagesEnd();
+
+    if (this.pages.length === TOTAL_SELECTOR_PAGES) return;
+    this.addPagesStart();
+  }
+
+  addPagesEnd(): void {
+    const lastPagePosition = this.pages.length - 1;
+    const lastPage = this.pages[lastPagePosition];
+
+    const isLastPage = lastPage === this.lastPage;
+    const isFull = this.pages.length === TOTAL_SELECTOR_PAGES;
+
+    if (isLastPage || isFull) return;
+
+    const newPage = lastPage + 1;
+    this.pages.push(newPage);
+
+    return this.addPagesEnd();
+  }
+
+  addPagesStart(): void {
+    const firstPage = this.pages[0];
+
+    const isFull = this.pages.length === TOTAL_SELECTOR_PAGES;
+    const isFirstPage = firstPage === FIRST_PAGE;
+
+    if (isFull || isFirstPage) return;
+
+    const newPage = firstPage - 1;
+    this.pages.unshift(newPage);
+
+    return this.addPagesStart();
+  }
+
+  goToPage(page: number) {
+    if (
+      page === this.currentPage ||
+      page > this.lastPage ||
+      page < FIRST_PAGE
+    ) {
+      return;
+    }
+    this.clickPage.next(page);
+  }
+}
